d7b6a37d590305d3a81efcaee34f7803a8ee7a0a
gromax4 <malevich.m.439-1@e.tusur.ru>
2023-12-14 20:25:14 +0300

    add slider and html of the news block

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,10 @@
 import burger_menu from './modules/burger_menu';
 import modal from './modules/modal';
+import slider from './modules/slider';
 import '../scss/global.scss'
 
 window.addEventListener('DOMContentLoaded', function() {
     burger_menu();
     modal();
+    slider();
 }); 
