e03ade5fb815ee6044f5271c38003d074f0d585e
gromax4 <malevich.m.439-1@e.tusur.ru>
2023-12-15 16:14:09 +0300

    fix: close the mobile menu when clicking on links

--- src/js/modules/burger_menu.js
+++ src/js/modules/burger_menu.js
@@ -8,12 +8,25 @@ function burger_menu(){
         menu.classList.toggle("header__menu_active");
     }); 
 
+    // вывод пунктов менб при наведении на навигационную ссылку содержащую подпункты
     navItems.forEach((elem) => {
         elem.addEventListener('mouseover', () => {
             elem.querySelector('.header__links').classList.add("header__links_active");
         });
         elem.addEventListener('mouseout', () => {
             elem.querySelector('.header__links').classList.remove("header__links_active");
+        });
+    });
+
+    // закрытие меню при клике на ссылку навигации
+    const links = document.querySelector('.header__nav').querySelectorAll('a');
+    links.forEach((elem) => {
+        elem.addEventListener('click', () => {
+            if (menu.classList.contains("header__menu_active"))
+            {
+                burger.classList.remove("burger_active");
+                menu.classList.remove("header__menu_active");
+            }
         });
     });
 }
